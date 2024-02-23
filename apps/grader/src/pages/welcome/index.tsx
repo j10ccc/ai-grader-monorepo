a98ac6fe84c6f5dc8a26311e7ef064d06da86b0c
j10c <blyb1739@gmail.com>
2024-02-23 21:31:39 +0800

    fix(welcome): remove welcome auth logic

--- apps/grader/src/pages/welcome/index.tsx
+++ apps/grader/src/pages/welcome/index.tsx
@@ -1,23 +1,4 @@
-import { useNavigate } from "@tanstack/react-router";
-import { useEffect } from "react";
-import useAuth from "@/hooks/use-auth";
-
 export default function WelcomePage() {
-  const { _hasHydrated, checkLoginExpire } = useAuth();
-  const navigate = useNavigate();
-
-  useEffect(() => {
-    if (_hasHydrated) {
-      const isExpired = checkLoginExpire();
-      // TODO: handle login by cookie
-      if (isExpired) {
-        navigate({ to: "/login" });
-      } else {
-        navigate({ to: "/dashboard" });
-      }
-    }
-  }, [_hasHydrated]);
-
   return (
     <main className="mx-auto my-0">
       <p>Welcome...</p>
